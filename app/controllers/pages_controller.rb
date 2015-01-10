13e1886c34209d8e054798dd6e0c635491515499
Bassem Shaker <bshakr@gmail.com>
2015-01-10 15:19:38 +0000

    Commit stats WIP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,10 @@ class PagesController < ApplicationController
 
   def about
     @instagram = Instagram.user_recent_media(INSTAGRAM_USER_ID, { count: 7 })
-    github = Github.new(oauth_token: '6358eb221b217687e6bf42291ecb43c53723bcb3')
-    repos = github.repos.all.select {|r| r.updated_at >= 4.weeks.ago }.map(&:name)
+    @github = Github.new(oauth_token: '6358eb221b217687e6bf42291ecb43c53723bcb3')
+    @repos = @github.repos.all.select {|r| r.updated_at >= 4.weeks.ago }.map(&:name)
     @commits = []
-    @commits += repos.map{ |r| github.repos.commits.all 'bshakr', r }.flatten
-
-
+    @commits += @repos.map{ |r| @github.repos.commits.all 'bshakr', r }.flatten
+    @activities = @repos.map{ |r| @github.repos.stats.code_frequency 'bshakr', r }
   end
 end
