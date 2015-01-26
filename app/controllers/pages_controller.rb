f6c0ecd6add5ca30a3d1025b3f8e45371fde28cb
Bassem Shaker <bshakr@gmail.com>
2015-01-26 23:36:26 +0000

    Remove instagram

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,6 @@ class PagesController < ApplicationController
   end
 
   def about
-    @instagram = Instagram.user_recent_media(INSTAGRAM_USER_ID, { count: 7 })
     @commits = Commit.where('created_at > ?', 1.month.ago)
     @total_lines_of_code = @commits.map{|c| c.total}.reduce(0){|sum, total| sum + total }
   end
